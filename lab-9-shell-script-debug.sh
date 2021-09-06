0d96b4c6ae51d8240046c5700660ce905bf5b9e7
cchang62 <thompson292@gmail.com>
2021-09-06 22:45:26 +0800

    add lab-9 shell script debug mode

---
+++ lab-9-shell-script-debug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ref. 
+# https://www.cyberciti.biz/tips/debugging-shell-script.html 
+# https://kknews.cc/zh-tw/code/n35a9pq.html
+
+_DEBUG="on"
+function DEBUG()
+{
+  [ "$_DEBUG" == "on" ] &&  $@
+}
+
+DEBUG echo 'Reading files'
+for i in *
+do
+  grep 'something' $i > /dev/null
+  [ $? -eq 0 ] && echo "Found in $i file"
+done
+DEBUG set -x
+a=2
+b=3
+c=$(( $a + $b ))
+DEBUG set +x
+echo "$a + $b = $c"
